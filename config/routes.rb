013e42283dc7ca1cd1c33f8d3f5d77e89bf5ebf1
tachimo <92985370+tachimo@users.noreply.github.com>
2022-09-11 09:30:09 +0900

    Add:Userモデルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: "home#top"
+
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
+  
+  resources :users, only: %i[index new create edit show update destroy]
 end

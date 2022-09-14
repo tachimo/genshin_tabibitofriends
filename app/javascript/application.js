576f323e96c441394b7f7fb47a46d9c56e16d1d4
tachimo <92985370+tachimo@users.noreply.github.com>
2022-09-14 21:18:08 +0900

    Fix:ホームページ用などの画像追加及び画像設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import * as bootstrap from "bootstrap"
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
